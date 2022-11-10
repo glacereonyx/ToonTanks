ad442fe592eee114f371ccf8f5c1a8745cc20424
Ben Mitchell <ben.mitchell@instantonline.nz>
2022-11-10 22:53:49 +1300

    Init Commit

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+ATank::ATank()
+{
+
+  SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+  SpringArm->SetupAttachment(RootComponent);
+
+  Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+  Camera->SetupAttachment(SpringArm);
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+  PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+}
+
+void ATank::Move(float Value)
+{
+  FVector DeltaLocation = FVector::ZeroVector;
+  DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
+
+  AddActorLocalOffset(DeltaLocation);
+}
